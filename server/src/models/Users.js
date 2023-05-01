6a8122eab8fdf84c0af200206769a581b024b5ee
Aadhar Sharma <aadhar@aadhar.lan>
2023-05-01 03:13:48 -0700

    p

--- server/src/models/Users.js
+++ server/src/models/Users.js
@@ -2,8 +2,9 @@ import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
     username: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    email: {type: String, required: true, unique: true}
+    profilePic: {type: String}
 });
 
 export const UserModel = mongoose.model('users', UserSchema);
