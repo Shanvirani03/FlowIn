1e2d8fb0dbadaa8e93cd7f339a56d0e25ca0cf2a
Shan Virani <shan@Shans-MBP.attlocal.net>
2023-06-01 04:40:46 -0700

    6/1 changes

--- server/src/routes/beats.js
+++ server/src/routes/beats.js
@@ -8,12 +8,32 @@ const router = express.router()
 
 
 router.post('/uploadBeat', verifyToken, (req, res) => {
+    
+    const { name, description, file } = req.body;
+
     try {
-
-    } catch {
-
+        if (!name || !description || !file) {
+            return res.status(422).json({ error: "Please complete all of the required fields." });
+        }
+        const beat = new BeatsModel({
+            name,
+            description,
+            file,
+            postedBy: req.user.id
+        });
+        beat.save()
+            .then(result => {
+                res.json({ beat: result });
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({ error: "An error occurred while saving the beat." });
+            });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: "An error occurred while uploading the beat." });
     }
-})
+});
 
 router.delete('/deleteBeat', verifyToken, (req, res) => {
     try {
